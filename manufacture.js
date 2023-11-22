f9ef2102eb8fbae8dd4545c6cbaadcc47bd377a2
Anaglyphic <97275120+Anaglyph-ic@users.noreply.github.com>
2023-11-22 17:17:36 +0100

    Add files via upload

--- manufacture.js
+++ manufacture.js
@@ -1,5 +1,9 @@
 import * as THREE from "three";
 
+import * as BufferGeometryUtils from "./lib/BufferGeometryUtils.js";
+
+import { ParametricGeometry } from "./lib/ParametricGeometry.js";
+
 //base materials
 const hatchMaterial = new THREE.MeshBasicMaterial({
   color: "white",
@@ -14,7 +18,7 @@ const lineMaterial = new THREE.LineBasicMaterial({
   color: "black",
 });
 
-const meshManufacture = (figures, mode) => {
+const meshManufacture = (figures, mode, param) => {
   const meshes = [];
 
   figures.forEach((fig) => {
@@ -27,14 +31,61 @@ const meshManufacture = (figures, mode) => {
 
       geometry = new THREE.ExtrudeGeometry(shape, fig.geometry.extrudeSettings);
 
-      //      geometry.center();
+      geometry.center();
+      edgeGeometry = new THREE.EdgesGeometry(geometry);
+    } else if (fig.geometry.type === "ParametricGeometry") {
+      geometry = new ParametricGeometry(
+        param[fig.geometry.args[0]],
+        fig.geometry.args[1],
+        fig.geometry.args[2]
+      );
+      delete geometry.parameters;
+
+      const bufferGeometry = new THREE.BufferGeometry();
+
+      bufferGeometry.setAttribute(
+        "position",
+        new THREE.BufferAttribute(
+          geometry.attributes.position.array,
+          geometry.attributes.position.itemSize,
+          geometry.attributes.position.Boolean
+        )
+      );
+
+      bufferGeometry.setAttribute(
+        "normal",
+        new THREE.BufferAttribute(
+          geometry.attributes.normal.array,
+          geometry.attributes.normal.itemSize,
+          geometry.attributes.normal.Boolean
+        )
+      );
+
+      bufferGeometry.setAttribute(
+        "uv",
+        new THREE.BufferAttribute(
+          geometry.attributes.uv.array,
+          geometry.attributes.uv.itemSize,
+          geometry.attributes.uv.Boolean
+        )
+      );
+
+      bufferGeometry.setIndex(
+        new THREE.BufferAttribute(
+          geometry.index.array,
+          geometry.index.itemSize,
+          geometry.index.Boolean
+        )
+      );
+
+      geometry = bufferGeometry;
+
       edgeGeometry = new THREE.EdgesGeometry(geometry);
     } else {
       // edge zfighting hack
 
       geometry = new THREE[fig.geometry.type](...fig.geometry.args);
       geometry.center();
-      //geometry = calcFaceDirection(geometry, camera);
 
       edgeGeometry = new THREE.EdgesGeometry(geometry);
     }
@@ -85,7 +136,7 @@ const meshManufacture = (figures, mode) => {
 };
 
 const addToScene = (scene, genfigs, mode) => {
-  const meshes = meshManufacture(genfigs.figures, mode);
+  const meshes = meshManufacture(genfigs.figures, mode, genfigs.parametric);
 
   if (mode === "Plottable") {
     const model = new THREE.Group();
@@ -94,7 +145,30 @@ const addToScene = (scene, genfigs, mode) => {
     // to get another perspective.
     model.rotation.x = Math.PI / 2;
 
-    const proj = buildLineProjection(model, meshes);
+    const geometries = [];
+
+    meshes.forEach((d) => {
+      const g = d.geometry;
+
+      for (const key in g.attributes) {
+        if (key !== "position") {
+          g.deleteAttribute(key);
+        }
+      }
+      g.index = null;
+
+      geometries.push(g);
+    });
+
+    const merged = BufferGeometryUtils.mergeGeometries(geometries, false);
+
+    const mesh = new THREE.Mesh(merged, hatchMaterial);
+
+    const runtime = 150;
+    const angle_threshold = 150;
+
+    const proj = buildLineProjection(model, [mesh], runtime, angle_threshold);
+
     scene.add(proj);
   } else {
     meshes.forEach((d) => scene.add(d));
